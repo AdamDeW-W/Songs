0f0066455eb6407f3a6be0a3339bfd31b79c5a18
Adam Dewitt <lowkeycoat1@gmail.com>
2024-02-25 11:58:07 -0600

    addings proper Songs.kt

--- src/Songs.kt
+++ src/Songs.kt
@@ -1,2 +1,18 @@
-class Songs {
+class Song ( val title: String, val artist: String)  {
+    fun play() {
+        println("playing the song $title by $artist")
+    }
+
+    fun stop() {
+        println("stopped playing $title")
+    }
+}
+
+fun main(args: Array<String>) {
+    val songOne = Song (" The Mesopotamians", "They Might Be Giants")
+    val songTwo = Song ("Going Underground", "The Jam")
+    val songThree = Song ("Make me Smile", "Steve Harley")
+    songTwo.play()
+    songTwo.stop ()
+    songThree.play()
 }
